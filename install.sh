7c1c7e20fc07c6873dec440ad04a7461475d425b
MadCat9958 <abolvah8@gmail.com>
2023-10-28 18:20:45 +0300

    Some changes

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-sudo pacman -S dunst kitty waybar wl-clipboard hyprpaper polkit-kde-agent breeze breeze-gtk grim slurp wofi swaylock nemo
-yay -S wlsunset hyprland-git 
+sudo pacman -S dunst kitty waybar wl-clipboard hyprpaper polkit-kde-agent breeze breeze-gtk grim slurp wofi swaylock nemo sddm
+yay -S wlsunset hyprland-git
+sudo systemctl enable sddm.service
+printf "Completed!\n[!!!] Please, restart your system [!!!]\n"
