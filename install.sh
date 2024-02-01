13827fcf6d9f0d020aebe627e629ad6104be172e
MadCat9958 <abolvah8@gmail.com>
2024-02-01 11:21:12 +0300

    Install required fonts

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-sudo pacman -S dunst kitty waybar wl-clipboard hyprpaper polkit-kde-agent breeze breeze-gtk grim slurp wofi swaylock nemo sddm bluez bluez-utils blueman && (yay -S wlsunset hyprland-git || paru -S wlsunset hyprland-git) && sudo systemctl enable sddm.service && echo "Completed!\n[!!!] Please, restart your system [!!!]\n"
+sudo pacman -S dunst kitty waybar wl-clipboard hyprpaper polkit-kde-agent breeze breeze-gtk grim slurp wofi swaylock nemo sddm bluez bluez-utils blueman noto-fonts noto-fonts-emoji noto-fonts-cjk otf-font-awesome && (yay -S wlsunset hyprland-git || paru -S wlsunset hyprland-git) && sudo systemctl enable sddm.service && echo "Completed!\n[!!!] Please, restart your system [!!!]\n"
+
